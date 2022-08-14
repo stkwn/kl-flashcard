de3fbaf02a7513c2891094dbcd3f21a909d6cf7b
stkwn <34761@qq.com>
2022-08-14 16:57:12 +1000

    remove empty string

--- src/component/WordPractice.jsx
+++ src/component/WordPractice.jsx
@@ -1,23 +1,37 @@
 import { useState } from "react";
 
 const WordPractice = (props) => {
-  console.log(props.wordList);
-  const wordListArray=props.wordList.trim().split(" ");
+  const wordListArrayTemp = props.wordList
+  .replace(/[\r\n]/g, " ")
+  .trim();
+  const wordListArray= wordListArrayTemp.split(" ").filter(w=>(w!== ''));
   const [word, setWord] = useState('开始');
+  const [wordIndex, setWordIndex] = useState(0);
   
-  const getRandomInt = () => {
+  const getRandomWord = () => {
    const selectedWord =
      wordListArray[Math.floor(Math.random() * wordListArray.length)];
     setWord(selectedWord);
   }
 
+  const getNormalWord =() =>{
+    const selectedWord = wordListArray[wordIndex];
+    setWordIndex(wordIndex+1);
+    if (wordIndex === wordListArray.length-1) setWordIndex(0);
+    setWord(selectedWord);
+  }  
     return (
       <div className="App">
         <header className="App-header">
           <p className="wordContent">{word}</p>
-          <button className="btn btn-primary" onClick={getRandomInt}>
-            换一个字
-          </button>
+          <div className="mb-3">
+            <button className="btn btn-primary m-3" onClick={getNormalWord}>
+              顺序练习
+            </button>
+            <button className="btn btn-primary m-3" onClick={getRandomWord}>
+              随机练习
+            </button>
+          </div>
         </header>
       </div>
     );
